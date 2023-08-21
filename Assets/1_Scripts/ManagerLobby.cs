ce5007ca540ddc4d8dd5d885165edd19149b95e2
LYS11234 <young990420@gmail.com>
2023-08-21 15:09:58 +0900

    PUN2 테스트 신 추

---
+++ Assets/1_Scripts/ManagerLobby.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Newtonsoft.Json;
+
+public class ManagerLobby : MonoBehaviourPunCallbacks
+{
+    private void Awake()
+    {
+        PhotonNetwork.NickName = UnityEngine.Random.Range(1000, 9999).ToString();
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void OnRoomCreate()
+    {
+        PhotonNetwork.CreateRoom("abc");
+    }
+
+    public void OnRoomJoin()
+    {
+        PhotonNetwork.JoinRoom("abc");
+    }
+
+    #region Photon
+    public override void OnConnected()
+    {
+        base.OnConnected();
+        Debug.Log("OnConnected");
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        Debug.Log("OnLeftRoom");
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        base.OnMasterClientSwitched(newMasterClient);
+        Debug.Log("newMasterClient : " + newMasterClient.NickName);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("OnCreatedRoom");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("OnJoinedLobby");
+    }
+
+    public override void OnLeftLobby()
+    {
+        base.OnLeftLobby();
+        Debug.Log("OnLeftLobby");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        Debug.Log("OnDisconnected" + cause);
+
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        base.OnRoomListUpdate(roomList);
+        Debug.Log("OnRoomListUpdate" + JsonConvert.SerializeObject(roomList));
+        //Debug.Log("OnRoomListUpdate" + JsonConvert.SerializeObject(roomList));
+        //  List<RoomInfo> a = JsonConvert.DeserializeObject<List<RoomInfo>>("");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("OnJoinedRoom");
+
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        Debug.Log("OnPlayerEnteredRoom " + newPlayer.NickName);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        Debug.Log("OnPlayerLeftRoom " + otherPlayer.NickName);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("OnConnectedToMaster");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnRoomPropertiesUpdate(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        base.OnRoomPropertiesUpdate(propertiesThatChanged);
+        Debug.Log("OnRoomPropertiesUpdate: " + JsonConvert.SerializeObject(propertiesThatChanged));
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        base.OnPlayerPropertiesUpdate(targetPlayer, changedProps);
+        Debug.Log("OnPlayerPropertiesUpdate: " + targetPlayer.NickName + " : " + JsonConvert.SerializeObject(changedProps));
+    }
+    #endregion
+}
---
+++ Assets/1_Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviourPun, IPunObservable
+{
+    PhotonView view;
+    Rigidbody myRigid;
+    public int applySpeed = 5;
+
+    Vector3 realPos;
+    Quaternion realRot;
+
+    public Transform myTransform;
+
+    void Awake()
+    {
+        myTransform = transform;
+        view = GetComponent<PhotonView>();
+        myRigid = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (view.IsMine)
+        {
+            Move();
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                view.RPC("Jump", RpcTarget.All, "abc", "def");
+            }
+            else if(Input.GetKeyDown(KeyCode.T))
+            {
+                PropertyUpdate();
+            }
+        }
+        else
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, realPos, 10 * Time.deltaTime);
+            myTransform.rotation = Quaternion.Lerp(myTransform.rotation, realRot, 10 * Time.deltaTime);
+        }
+    }
+
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = myTransform.right * _moveDirX;
+        Vector3 _moveVertical = myTransform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
+
+        myRigid.MovePosition(myTransform.position + _velocity * Time.deltaTime);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext(myTransform.position);
+            stream.SendNext(myTransform.rotation);
+        } 
+        else
+        {
+            realPos = (Vector3)stream.ReceiveNext();
+            realRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    [PunRPC]
+    public void Jump(string a, string b)
+    {
+        Debug.Log($"Jump {a}, {b}, " + view.name);
+    }
+
+    public void PropertyUpdate()
+    {
+        ExitGames.Client.Photon.Hashtable initialProps = new ExitGames.Client.Photon.Hashtable() { { "isReady", false } };
+        PhotonNetwork.LocalPlayer.SetCustomProperties(initialProps);
+    }
+}
